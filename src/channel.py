3426308f13a4ca7e3d9e37ff5a107e22a8a8256b
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-11-16 17:58:41 +0300

    Added new dunder-methods

--- src/channel.py
+++ src/channel.py
@@ -15,9 +15,30 @@ class Channel:
         self.title = self.channel_info['items'][0]['snippet']['title']
         self.description = self.channel_info['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.subscriber_count = int(self.channel_info['items'][0]['statistics']['subscriberCount'])
         self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
         self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
 
     @property
     def channel_id(self) -> str:
