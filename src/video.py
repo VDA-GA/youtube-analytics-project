04176ace4fa86d456d28e3c185d9019814f4c5ec
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-11-26 23:26:49 +0300

    Added classes Video and PLVideo

---
+++ src/video.py
@@ -0,0 +1,31 @@
+from googleapiclient.discovery import build
+import os
+
+
+class Video:
+
+    def __init__(self, video_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        __api_key: str = os.getenv('YT_API_KEY')
+        self.video_id = video_id
+        youtube = build('youtube', 'v3', developerKey=__api_key)
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+
+        self.video_title: str = video_response['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+
+    def __str__(self):
+        return self.video_title
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.video_title}', {self.url}, {self.view_count}, {self.like_count})"
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
