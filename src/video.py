1925ca1239d7433bebe8b884dec0adf7eef36eb3
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-12-03 21:42:48 +0300

    Add exceptions

--- src/video.py
+++ src/video.py
@@ -2,25 +2,31 @@ from src.channel import APIMixin
 
 
 class Video(APIMixin):
-
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.video_id = video_id
         youtube = APIMixin.get_service()
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + self.video_id
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        video_response = (
+            youtube.videos()
+            .list(part="snippet,statistics,contentDetails,topicDetails", id=video_id)
+            .execute()
+        )
+        try:
+            self.title: str = video_response["items"][0]["snippet"]["title"]
+            self.url = "https://www.youtube.com/watch?v=" + self.video_id
+            self.view_count: int = video_response["items"][0]["statistics"]["viewCount"]
+            self.like_count: int = video_response["items"][0]["statistics"]["likeCount"]
+        except (IndexError, Exception):
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.video_title}', {self.url}, {self.view_count}, {self.like_count})"
+        return f"{self.__class__.__name__}('{self.title}', {self.url}, {self.view_count}, {self.like_count})"
 
 
 class PLVideo(Video):
